1c8153d2c14fdfed0c3c8d2ecb355fee91701396
Abhishek Raj <abhishekcultivates@gmail.com>
2024-09-22 23:23:57 +0530

    updated code

--- heap.js
+++ heap.js
@@ -1,71 +1,82 @@
-export { BinaryHeap }
+export class BinaryHeap {
+  constructor() {
+    this.heap = [];
+  }
 
-class BinaryHeap {
+  // Insert a new value and ensure heap property
+  insert(value) {
+    this.heap.push(value);
+    this._bubbleUp();
+  }
 
-    constructor() {
-        this.heap = [];
+  // Return the size of the heap
+  size() {
+    return this.heap.length;
+  }
+
+  // Check if the heap is empty
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  // Bubble up the last inserted element to maintain max-heap property
+  _bubbleUp() {
+    let index = this.size() - 1;
+
+    while (index > 0) {
+      const element = this.heap[index];
+      const parentIndex = Math.floor((index - 1) / 2);
+      const parent = this.heap[parentIndex];
+
+      if (parent[0] >= element[0]) break; // Max-heap property is satisfied
+      [this.heap[index], this.heap[parentIndex]] = [
+        this.heap[parentIndex],
+        this.heap[index],
+      ];
+      index = parentIndex;
+    }
+  }
+
+  // Extract the max value (root) and maintain heap property
+  extractMax() {
+    if (this.isEmpty()) return null;
+
+    const max = this.heap[0];
+    const end = this.heap.pop();
+
+    if (!this.isEmpty()) {
+      this.heap[0] = end;
+      this._sinkDown(0);
     }
 
-    insert(value) {
-        this.heap.push(value);
-        this.bubbleUp();
+    return max;
+  }
+
+  // Sink down the root element to maintain max-heap property
+  _sinkDown(index) {
+    const length = this.size();
+    const element = this.heap[index];
+
+    while (true) {
+      const left = 2 * index + 1;
+      const right = 2 * index + 2;
+      let largest = index;
+
+      if (left < length && this.heap[left][0] > this.heap[largest][0]) {
+        largest = left;
+      }
+
+      if (right < length && this.heap[right][0] > this.heap[largest][0]) {
+        largest = right;
+      }
+
+      if (largest === index) break; // Max-heap property is satisfied
+
+      [this.heap[index], this.heap[largest]] = [
+        this.heap[largest],
+        this.heap[index],
+      ];
+      index = largest;
     }
-
-    size() {
-        return this.heap.length;
-    }
-
-    empty(){
-        return ( this.size()===0 );
-    }
-
-    //using iterative approach
-    bubbleUp() {
-        let index = this.size() - 1;
-
-        while (index > 0) {
-            let element = this.heap[index],
-                parentIndex = Math.floor((index - 1) / 2),
-                parent = this.heap[parentIndex];
-
-            if (parent[0] >= element[0]) break;
-            this.heap[index] = parent;
-            this.heap[parentIndex] = element;
-            index = parentIndex
-        }
-    }
-
-    extractMax() {
-        const max = this.heap[0];
-        const tmp = this.heap.pop();
-        if(!this.empty()) {
-            this.heap[0] = tmp;
-            this.sinkDown(0);
-        }
-        return max;
-    }
-
-    sinkDown(index) {
-
-        let left = 2 * index + 1,
-            right = 2 * index + 2,
-            largest = index;
-        const length = this.size();
-
-        // console.log(this.heap[left], left, length, this.heap[right], right, length, this.heap[largest]);
-
-        if (left < length && this.heap[left][0] > this.heap[largest][0]) {
-            largest = left
-        }
-        if (right < length && this.heap[right][0] > this.heap[largest][0]) {
-            largest = right
-        }
-        // swap
-        if (largest !== index) {
-            let tmp = this.heap[largest];
-            this.heap[largest] = this.heap[index];
-            this.heap[index] = tmp;
-            this.sinkDown(largest)
-        }
-    }
+  }
 }
--- huffman.js
+++ huffman.js
@@ -1,163 +1,228 @@
+import { BinaryHeap } from "./heap.js";
 
+export class HuffmanCoder {
+  /**
+   * Convert the Huffman tree node into a string format
+   * @param {Array} node - Huffman tree node
+   * @returns {string} - Stringified node
+   */
+  stringify(node) {
+    return typeof node[1] === "string"
+      ? `'${node[1]}`
+      : `0${this.stringify(node[1][0])}1${this.stringify(node[1][1])}`;
+  }
 
-import { BinaryHeap } from './heap.js';
-
-export { HuffmanCoder }
-
-class HuffmanCoder{
-
-    stringify(node){
-        if(typeof(node[1])==="string"){
-            return '\''+node[1];
-        }
-
-        return '0' + this.stringify(node[1][0]) + '1' + this.stringify(node[1][1]);
+  /**
+   * Display the Huffman tree structure
+   * @param {Array} node - Huffman tree node
+   * @param {boolean} modify - Whether to modify the node structure for display
+   * @param {number} index - Index of the current node in the tree
+   * @returns {string} - Tree structure as a string
+   */
+  display(node, modify = false, index = 1) {
+    if (modify) {
+      node = ["", node];
+      if (node[1].length === 1) {
+        node[1] = node[1][0];
+      }
     }
 
-    display(node, modify, index=1){
-        if(modify){
-            node = ['',node];
-            if(node[1].length===1)
-                node[1] = node[1][0];
-        }
-
-        if(typeof(node[1])==="string"){
-            return String(index) + " = " + node[1];
-        }
-
-        let left = this.display(node[1][0], modify, index*2);
-        let right = this.display(node[1][1], modify, index*2+1);
-        let res = String(index*2)+" <= "+index+" => "+String(index*2+1);
-        return res + '\n' + left + '\n' + right;
+    if (typeof node[1] === "string") {
+      return `${index} = ${node[1]}`;
     }
 
-    destringify(data){
-        let node = [];
-        if(data[this.ind]==='\''){
-            this.ind++;
-            node.push(data[this.ind]);
-            this.ind++;
-            return node;
-        }
+    const left = this.display(node[1][0], modify, index * 2);
+    const right = this.display(node[1][1], modify, index * 2 + 1);
+    return `${index * 2} <= ${index} => ${index * 2 + 1}\n${left}\n${right}`;
+  }
 
-        this.ind++;
-        let left = this.destringify(data);
-        node.push(left);
-        this.ind++;
-        let right = this.destringify(data);
-        node.push(right);
-
-        return node;
+  /**
+   * Convert a string back into a Huffman tree node
+   * @param {string} data - The stringified tree
+   * @returns {Array} - Destringified node
+   */
+  destringify(data) {
+    const node = [];
+    if (data[this.ind] === "'") {
+      this.ind++;
+      node.push(data[this.ind++]);
+      return node;
     }
 
-    getMappings(node, path){
-        if(typeof(node[1])==="string"){
-            this.mappings[node[1]] = path;
-            return;
-        }
+    this.ind++;
+    const left = this.destringify(data);
+    node.push(left);
+    this.ind++;
+    const right = this.destringify(data);
+    node.push(right);
 
-        this.getMappings(node[1][0], path+"0");
-        this.getMappings(node[1][1], path+"1");
+    return node;
+  }
+
+  /**
+   * Get the binary mappings of characters based on the Huffman tree
+   * @param {Array} node - Huffman tree node
+   * @param {string} path - Current binary path in the tree
+   */
+  getMappings(node, path = "") {
+    if (typeof node[1] === "string") {
+      this.mappings[node[1]] = path;
+      return;
     }
 
-    encode(data){
+    this.getMappings(node[1][0], `${path}0`);
+    this.getMappings(node[1][1], `${path}1`);
+  }
 
-        this.heap = new BinaryHeap();
+  /**
+   * Encode the given data using Huffman encoding
+   * @param {string} data - Input string to encode
+   * @returns {[string, string, string]} - Encoded result, tree structure, and compression info
+   */
+  encode(data) {
+    this.heap = new BinaryHeap();
+    const frequencyMap = this.buildFrequencyMap(data);
 
-        const mp = new Map();
-        for(let i=0;i<data.length;i++){
-            if(data[i] in mp){
-                mp[data[i]] = mp[data[i]] + 1;
-            } else{
-                mp[data[i]] = 1;
-            }
-        }
+    frequencyMap.forEach((frequency, char) => {
+      this.heap.insert([-frequency, char]);
+    });
 
-        for(const key in mp){
-            this.heap.insert([-mp[key], key]);
-        }
-
-        while(this.heap.size() > 1){
-            const node1 = this.heap.extractMax();
-            const node2 = this.heap.extractMax();
-
-            const node = [node1[0]+node2[0],[node1,node2]];
-            this.heap.insert(node);
-        }
-        const huffman_encoder = this.heap.extractMax();
-
-        this.mappings = {};
-        this.getMappings(huffman_encoder, "");
-
-        let binary_string = "";
-        for(let i=0;i<data.length;i++) {
-            binary_string = binary_string + this.mappings[data[i]];
-        }
-
-        let rem = (8 - binary_string.length%8)%8;
-        let padding = "";
-        for(let i=0;i<rem;i++)
-            padding = padding + "0";
-        binary_string = binary_string + padding;
-
-        let result = "";
-        for(let i=0;i<binary_string.length;i+=8){
-            let num = 0;
-            for(let j=0;j<8;j++){
-                num = num*2 + (binary_string[i+j]-"0");
-            }
-            result = result + String.fromCharCode(num);
-        }
-
-        let final_res = this.stringify(huffman_encoder) + '\n' + rem + '\n' + result;
-        let info = "Compression Ratio : " + data.length/final_res.length;
-        info = "Compression complete and file sent for download" + '\n' + info;
-        return [final_res, this.display(huffman_encoder, false), info];
+    while (this.heap.size() > 1) {
+      const node1 = this.heap.extractMax();
+      const node2 = this.heap.extractMax();
+      this.heap.insert([node1[0] + node2[0], [node1, node2]]);
     }
 
-    decode(data){
-        data = data.split('\n');
-        if(data.length===4){
-            // Handling new line
-            data[0] = data[0] + '\n' + data[1];
-            data[1] = data[2];
-            data[2] = data[3];
-            data.pop();
-        }
+    const huffmanTree = this.heap.extractMax();
+    this.mappings = {};
+    this.getMappings(huffmanTree);
 
-        this.ind = 0;
-        const huffman_decoder = this.destringify(data[0]);
-        const text = data[2];
+    const binaryString = this.convertToBinaryString(data);
+    const [paddedBinaryString, rem] = this.addPadding(binaryString);
+    const encodedResult = this.binaryStringToText(paddedBinaryString);
+    const compressedData = `${this.stringify(
+      huffmanTree
+    )}\n${rem}\n${encodedResult}`;
+    const compressionInfo = `Compression complete and file sent for download\nCompression Ratio: ${(
+      data.length / compressedData.length
+    ).toFixed(2)}`;
 
-        let binary_string = "";
-        for(let i=0;i<text.length;i++){
-            let num = text[i].charCodeAt(0);
-            let bin = "";
-            for(let j=0;j<8;j++){
-                bin = num%2 + bin;
-                num = Math.floor(num/2);
-            }
-            binary_string = binary_string + bin;
-        }
-        binary_string = binary_string.substring(0,binary_string.length-data[1]);
+    return [compressedData, this.display(huffmanTree, false), compressionInfo];
+  }
 
-        console.log(binary_string.length);
+  /**
+   * Decode the given Huffman encoded data
+   * @param {string} data - Encoded data
+   * @returns {[string, string, string]} - Decoded text, tree structure, and decompression info
+   */
+  decode(data) {
+    const [treeString, padding, encodedText] = this.parseEncodedData(data);
+    this.ind = 0;
+    const huffmanTree = this.destringify(treeString);
 
-        let res = "";
-        let node = huffman_decoder;
-        for(let i=0;i<binary_string.length;i++){
-            if(binary_string[i]==='0'){
-                node = node[0];
-            } else{
-                node = node[1];
-            }
+    const binaryString = this.textToBinaryString(encodedText).slice(
+      0,
+      -parseInt(padding)
+    );
+    const decodedText = this.decodeBinaryString(binaryString, huffmanTree);
 
-            if(typeof(node[0])==="string"){
-                res += node[0];
-                node = huffman_decoder;
-            }
-        }
-        let info = "Decompression complete and file sent for download";
-        return [res, this.display(huffman_decoder, true), info];
+    return [
+      decodedText,
+      this.display(huffmanTree, true),
+      "Decompression complete and file sent for download",
+    ];
+  }
+
+  // Helper functions
+
+  /**
+   * Build a frequency map for the input data
+   * @param {string} data - Input data
+   * @returns {Map<string, number>} - Character frequency map
+   */
+  buildFrequencyMap(data) {
+    const frequencyMap = new Map();
+    for (const char of data) {
+      frequencyMap.set(char, (frequencyMap.get(char) || 0) + 1);
     }
+    return frequencyMap;
+  }
+
+  /**
+   * Convert data to a binary string using the Huffman mappings
+   * @param {string} data - Input data
+   * @returns {string} - Binary string representation of the data
+   */
+  convertToBinaryString(data) {
+    return data
+      .split("")
+      .map((char) => this.mappings[char])
+      .join("");
+  }
+
+  /**
+   * Add padding to the binary string to make its length a multiple of 8
+   * @param {string} binaryString - Binary string
+   * @returns {[string, number]} - Padded binary string and the number of padding bits
+   */
+  addPadding(binaryString) {
+    const rem = (8 - (binaryString.length % 8)) % 8;
+    return [binaryString + "0".repeat(rem), rem];
+  }
+
+  /**
+   * Convert a binary string to its text representation
+   * @param {string} binaryString - Binary string
+   * @returns {string} - Text representation
+   */
+  binaryStringToText(binaryString) {
+    return binaryString
+      .match(/.{1,8}/g)
+      .map((byte) => String.fromCharCode(parseInt(byte, 2)))
+      .join("");
+  }
+
+  /**
+   * Convert the encoded text back into a binary string
+   * @param {string} text - Encoded text
+   * @returns {string} - Binary string
+   */
+  textToBinaryString(text) {
+    return [...text]
+      .map((char) => char.charCodeAt(0).toString(2).padStart(8, "0"))
+      .join("");
+  }
+
+  /**
+   * Parse encoded data into tree, padding, and encoded text
+   * @param {string} data - Encoded data
+   * @returns {[string, string, string]} - Tree string, padding, and encoded text
+   */
+  parseEncodedData(data) {
+    const lines = data.split("\n");
+    return lines.length === 4
+      ? [`${lines[0]}\n${lines[1]}`, lines[2], lines[3]]
+      : [lines[0], lines[1], lines[2]];
+  }
+
+  /**
+   * Decode a binary string using the Huffman tree
+   * @param {string} binaryString - Binary string
+   * @param {Array} huffmanTree - Huffman tree
+   * @returns {string} - Decoded text
+   */
+  decodeBinaryString(binaryString, huffmanTree) {
+    let decodedText = "";
+    let node = huffmanTree;
+
+    for (const bit of binaryString) {
+      node = bit === "0" ? node[0] : node[1];
+      if (typeof node[0] === "string") {
+        decodedText += node[0];
+        node = huffmanTree;
+      }
+    }
+
+    return decodedText;
+  }
 }
